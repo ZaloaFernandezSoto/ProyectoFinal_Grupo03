2f536949dcb31a1d4bdb7a71501b4dbd13f2aa51
ZaloaFernandezSoto <144669435+ZaloaFernandezSoto@users.noreply.github.com>
2025-12-01 19:42:29 +0100

    Repo

---
+++ 3 App/app.py
@@ -0,0 +1,8 @@
+#Fase 3: Aplicación (Streamlit)
+#Estructura de la App (app.py):
+#Configurar el menú lateral para navegar entre "Análisis de Datos", "Entrenamiento" y "Monitorización en Tiempo Real"
+#Pestaña de Exploración (EDA Interactivo):
+#Migrar los gráficos del notebook a Streamlit. Permitir filtrar por "Tipo de Fallo" (ej. Fallo 1 vs. Normal) para ver cómo reaccionan las variables.
+#Pestaña de Monitorización (Conexión con BentoML):
+#Crear una interfaz que simule la llegada de datos cada 3 minutos.
+#Conectar el botón de "Analizar Estado Actual" a la API local de BentoML creada en el paso 7.
